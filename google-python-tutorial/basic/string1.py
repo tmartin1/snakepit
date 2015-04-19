3f551f73ce2b39cb17e5ba729968d2be898060bf
Tim Martin <timothy.m.martin@gmail.com>
2015-04-18 17:28:43 -0700

    string manipulation

--- google-python-tutorial/basic/string1.py
+++ google-python-tutorial/basic/string1.py
@@ -37,7 +37,10 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-  # +++your code here+++
+  if len(s) < 2:
+    print ''
+  else:
+    print s[:2] + s[-2:]
   return
 
 
