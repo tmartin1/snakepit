fdd0697e5861c13dde653be42f6630a690a63fd4
Tim Martin <timothy.m.martin@gmail.com>
2015-04-21 18:03:30 -0700

    App can fetch all beers from db and display as a list on index.html

---
+++ BeerDB/client/index.js
@@ -0,0 +1,9 @@
+// Look for beer in database.
+function findBeer() {
+
+};
+
+// Add a new beer to the database.
+function addBeer() {
+
+};
--- BeerDB/static/index.js
+++ BeerDB/static/index.js
@@ -1,6 +1,28 @@
-// Look for beer in database.
-function findBeer() {
+// Retreive all beers in the database.
+function getAllBeers() {
+  $('singleBeer').hide();
+  var beerList = $('<ul></ul>');
+  var allBeers = $.get("http://localhost:5000/beers/all", function(data) {
+    data = jQuery.parseJSON(data);
+    console.log(data);
+    for (var i=0, n=data.length; i<n; i++) {
+      beerList.append($('<li></li>').text(data[i].name))
+    }
+  });
+  $(".beerList").show().append(beerList);
+};
 
+// Displays a specific beer object to the user.
+function displayBeer(beer) {
+  $(".beerList").hide();
+  // Assign beer display stuff
+  $('.singleBeer').show();
+};
+
+// Look for a specific beer in database.
+function findBeer() {
+  $(".beerList").hide();
+  beer = jQuery.parseJSON()
 };
 
 // Add a new beer to the database.
